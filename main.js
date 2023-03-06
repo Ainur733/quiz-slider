db117f91803ae6ca24f05de047d2aab395ea9eae
Ainur733 <Ainur733@gmail.com>
2023-03-06 23:50:26 +0500

    create next and prev button

---
+++ main.js
@@ -0,0 +1,87 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const container = document.querySelector(".answers")
+    const nextButton = document.querySelector(".next")
+    const prevButton = document.querySelector(".prev")
+    const itemCount = document.querySelectorAll(".group").length
+    const progressBar = document.querySelector(".progress-bar_inner")
+    const itemWidth = container.clientWidth
+    let position = 0
+    let percentOneStep = 100 / itemCount
+    let percentStep = percentOneStep
+    let stepCount = 1
+    let countQuestionsOneStep = 2
+    let countQuestions = countQuestionsOneStep
+    let totalQuestions = countQuestions * itemCount
+
+
+    prevButton.style.display = "none"
+    progressBar.style.width = ""+ percentStep + "%"
+    progressBar.innerHTML = ""+ percentStep + "% ("+ countQuestions +"/" + totalQuestions + ")"
+
+
+    nextButton.addEventListener('click', function () {
+        if ((itemWidth * itemCount) - itemWidth - itemWidth <= position) {
+            nextButton.style.display = "none"
+        }
+        prevButton.style.display = "block"
+        position += itemWidth
+        container.style.transform += ("translateX(-" + itemWidth + "px")
+        nextStep()
+
+    })
+
+    prevButton.addEventListener('click', function () {
+        nextButton.style.display = "block"
+        if (position - itemWidth === 0) {
+            prevButton.style.display = "none"
+        }
+
+        position -= itemWidth
+        container.style.transform += ("translateX(" + itemWidth + "px")
+        prevStep()
+    })
+
+    function nextStep() {
+        percentStep += percentOneStep
+        countQuestions += countQuestionsOneStep
+
+        progressBar.style.width = ""+ percentStep + "%"
+        progressBar.innerHTML = ""+ percentStep + "% ("+ countQuestions +"/" + totalQuestions + ")"
+    }
+
+    function prevStep() {
+        percentStep -= percentOneStep
+        countQuestions -= countQuestionsOneStep
+
+        progressBar.style.width = ""+ percentStep + "%"
+        progressBar.innerHTML = ""+ percentStep + "% ("+ countQuestions +"/" + totalQuestions + ")"
+    }
+
+    // prevButton.style.display = "none"
+    //
+    // nextButton.addEventListener('click', function () {
+    //     if ((itemWidth * itemCount) - itemWidth - itemWidth <= position) {
+    //         nextButton.style.display = "none"
+    //         prevButton.style.display = "block"
+    //     }
+    //     prevButton.style.display = "block"
+    //     position += itemWidth
+    //     container.style.transform += ("translateX(-" + itemWidth + "px")
+    //
+    // })
+    //
+    // prevButton.addEventListener('click', function () {
+    //
+    //     if ((itemWidth * itemCount) + itemWidth >= position) {
+    //         prevButton.style.display = "none"
+    //         nextButton.style.display = "block"
+    //     }else {
+    //         nextButton.style.display = "block"
+    //     }
+    //
+    //     position -= itemWidth
+    //     container.style.transform += ("translateX(" + itemWidth + "px")
+    // })
+
+
+});
